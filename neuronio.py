aae9867ebd26b35a6f83bcf24f7c474aa14e7758
Makerjr <92755114+Makerjunior@users.noreply.github.com>
2023-04-25 22:40:31 -0300

    Initial commit

---
+++ neuronio.py
@@ -0,0 +1,77 @@
+'''
+Para criar um neurônio capaz de aprender com textos, precisamos definir uma forma de representar as palavras como
+entradas numéricas para o neurônio. Uma possível abordagem é usar a técnica de vetorização de texto,
+que transforma cada palavra em um vetor de números.
+
+Aqui está um exemplo de como podemos modificar a classe Neuronio para incluir essa funcionalidade:
+'''
+
+from tika import parser
+import  random
+import  numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+class Neuronio:
+    def __init__(self, num_entradas):
+        # atributos do neurônio
+        self.pesos = [random.uniform(-1, 1) for _ in range(num_entradas)]
+        self.bias = random.uniform(-1, 1)
+
+    def ativacao(self, entradas):
+        # calcula a soma ponderada das entradas e pesos + bias
+        somatorio = sum(
+            [x * w for x, w in zip(entradas, self.pesos)]) + self.bias
+        # aplica função de ativação (sigmoid)
+        resultado = 1 / (1 + pow(2.71828, -somatorio))
+        return resultado
+
+
+class NeuronioTexto:
+    def __init__(self, tam_vetorizacao):
+        # atributos do neurônio
+        self.pesos = np.array([random.uniform(-1, 1)
+                              for _ in range(tam_vetorizacao)])
+        self.bias = random.uniform(-1, 1)
+
+    def ativacao(self, vetor_texto):
+        # calcula a soma ponderada do vetor de texto e pesos + bias
+        somatorio = np.dot(vetor_texto, self.pesos) + self.bias
+        # aplica função de ativação (sigmoid)
+        resultado = 1 / (1 + pow(2.71828, -somatorio))
+        return resultado
+
+    def treinar(self, vetor_texto, saida_desejada, taxa_aprendizado):
+        # faz a predição para o vetor de entrada
+        predicao = self.ativacao(vetor_texto)
+        # calcula o erro
+        erro = saida_desejada - predicao
+        # atualiza os pesos e o bias
+        self.pesos += taxa_aprendizado * erro * vetor_texto
+        self.bias += taxa_aprendizado * erro
+
+'''
+Aqui está um exemplo de como podemos modificar a classe Neuronio para incluir essa funcionalidade:
+
+'''
+raw = parser.from_file('livro.pdf')
+#print(raw['content'])
+
+# criando um objeto NeuronioTexto com um vetor de tamanho 100
+neuronio_texto = NeuronioTexto(100)
+
+# definindo um texto
+texto = raw['content']
+
+# vetorizando o texto
+vetorizador = CountVectorizer(max_features=100)
+vetor_texto = vetorizador.fit_transform([texto]).toarray()
+#print(vetor_texto[0][0])
+# obtendo a saída do neurônio para o vetor de texto dado
+saida_texto = neuronio_texto.ativacao(vetor_texto[0][0])
+
+print("Vetor de saida do neurônio :")
+print(saida_texto)
+
+for vetor in vetor_texto:
+    saida_texto = neuronio_texto.ativacao(vetor)
+    saida_int = int(round(saida_texto))
+    print("\n\nSaida do neurônio em inteiro :"+str(saida_int))
